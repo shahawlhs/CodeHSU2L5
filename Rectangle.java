e70583b6b42cfe29f10ecf2c0e440c9755ee050f
smithb <smithb@wlwv.k12.or.us>
2022-10-04 08:15:00 -0700

    initial commit

---
+++ Rectangle.java
@@ -0,0 +1,35 @@
+public class Rectangle
+{
+    private int width;
+    private int height;
+    
+    /**
+     * This is the constructor to create a Rectangle.
+     * To create a Rectangle we need to know its width
+     * and height.
+     */
+    
+    //complete the constructor
+    public Rectangle(int rectWidth, int rectHeight)
+    {
+        
+    }
+    
+    //create a printArea method
+    
+    
+    //setter method, need to be able to set width and height
+    
+    
+    
+    
+    /**
+     * This is the toString method. It returns a String
+     * representation of the object. See what happens
+     * if you print out the object without a toString.
+     */
+    public String toString()
+    {
+        return "Rectangle with width: " + width + " and height: " + height;
+    }
+}
